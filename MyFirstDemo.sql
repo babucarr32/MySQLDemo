b4c8265b8ed44420aa695d208b1f3ecf8f139a8e
babucarr32 <baboucarry@gmail.com>
2022-08-20 14:57:36 +0000

    practicing in gomindz

--- MyFirstDemo.sql
+++ MyFirstDemo.sql
@@ -181,19 +181,13 @@
 -- FROM products p
 -- LEFT JOIN order_items oi ON p.product_id = oi.product_id
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+USE store;
+SELECT c.first_name, c.last_name, o.order_date, p.name,  oi.quantity
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON oi.order_id = o.order_id
+JOIN products p ON p.product_id = oi.product_id
+ORDER BY c.first_name
 
 
 
